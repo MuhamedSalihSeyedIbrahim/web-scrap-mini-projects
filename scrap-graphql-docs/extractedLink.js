621f7f2b074878ceef1c296914ebe24fcae473da
Muhamed Salih SI <30931525+MuhamedSalihSeyedIbrahim@users.noreply.github.com>
2020-05-20 23:59:55 +0530

    Update extractedLink.js

--- scrap-graphql-docs/extractedLink.js
+++ scrap-graphql-docs/extractedLink.js
@@ -1,68 +1,148 @@
-exports.link= {
+exports.link = {
+  graphql_js_docs: {
     "Part-0-Getting-Started": "https://graphql.org/graphql-js/",
-    "Part-1-Running-Express+GraphQL": "https://graphql.org/graphql-js/running-an-express-graphql-server/",
+    "Part-1-Running-Express+GraphQL":
+      "https://graphql.org/graphql-js/running-an-express-graphql-server/",
     "Part-2-GraphQL-Clients": "https://graphql.org/graphql-js/graphql-clients/",
     "Part-3-Basic-Types": "https://graphql.org/graphql-js/basic-types/",
-    "Part-4-Passing-Arguments": "https://graphql.org/graphql-js/passing-arguments/",
+    "Part-4-Passing-Arguments":
+      "https://graphql.org/graphql-js/passing-arguments/",
     "Part-5-Object-Types": "https://graphql.org/graphql-js/object-types/",
-    "Part-6-Mutations-and-Input-Types": "https://graphql.org/graphql-js/mutations-and-input-types/",
-    "Part-7-Authentication&Middleware": "https://graphql.org/graphql-js/authentication-and-express-middleware/",
-    "Part-8-Constructing-Types": "https://graphql.org/graphql-js/constructing-types/",
+    "Part-6-Mutations-and-Input-Types":
+      "https://graphql.org/graphql-js/mutations-and-input-types/",
+    "Part-7-Authentication&Middleware":
+      "https://graphql.org/graphql-js/authentication-and-express-middleware/",
+    "Part-8-Constructing-Types":
+      "https://graphql.org/graphql-js/constructing-types/",
     "Part-9-express-graphql": "https://graphql.org/graphql-js/express-graphql/",
-    "Part-10-graphqlHTTP": "https://graphql.org/graphql-js/express-graphql/#graphqlhttp",
+    "Part-10-graphqlHTTP":
+      "https://graphql.org/graphql-js/express-graphql/#graphqlhttp",
     "Part-11-graphql": "https://graphql.org/graphql-js/graphql/",
     "Part-12-graphql": "https://graphql.org/graphql-js/graphql/#graphql",
     "Part-13-graphql-error": "https://graphql.org/graphql-js/error/",
     "Part-14-formatError": "https://graphql.org/graphql-js/error/#formaterror",
-    "Part-15-GraphQLError": "https://graphql.org/graphql-js/error/#graphqlerror",
-    "Part-16-locatedError": "https://graphql.org/graphql-js/error/#locatederror",
+    "Part-15-GraphQLError":
+      "https://graphql.org/graphql-js/error/#graphqlerror",
+    "Part-16-locatedError":
+      "https://graphql.org/graphql-js/error/#locatederror",
     "Part-17-syntaxError": "https://graphql.org/graphql-js/error/#syntaxerror",
     "Part-18-graphql-execution": "https://graphql.org/graphql-js/execution/",
     "Part-19-execute": "https://graphql.org/graphql-js/execution/#execute",
     "Part-20-graphql-language": "https://graphql.org/graphql-js/language/",
     "Part-21-BREAK": "https://graphql.org/graphql-js/language/#break",
-    "Part-22-getLocation": "https://graphql.org/graphql-js/language/#getlocation",
+    "Part-22-getLocation":
+      "https://graphql.org/graphql-js/language/#getlocation",
     "Part-23-Kind": "https://graphql.org/graphql-js/language/#kind",
     "Part-24-lex": "https://graphql.org/graphql-js/language/#lex",
     "Part-25-parse": "https://graphql.org/graphql-js/language/#parse",
     "Part-26-parseValue": "https://graphql.org/graphql-js/language/#parsevalue",
-    "Part-27-printSource": "https://graphql.org/graphql-js/language/#printsource",
+    "Part-27-printSource":
+      "https://graphql.org/graphql-js/language/#printsource",
     "Part-28-visit": "https://graphql.org/graphql-js/language/#visit",
     "Part-29-graphql-type": "https://graphql.org/graphql-js/type/",
     "Part-30-getNamedType": "https://graphql.org/graphql-js/type/#getnamedtype",
-    "Part-31-getNullableType": "https://graphql.org/graphql-js/type/#getnullabletype",
-    "Part-32-GraphQLBoolean": "https://graphql.org/graphql-js/type/#graphqlboolean",
-    "Part-33-GraphQLEnumType": "https://graphql.org/graphql-js/type/#graphqlenumtype",
+    "Part-31-getNullableType":
+      "https://graphql.org/graphql-js/type/#getnullabletype",
+    "Part-32-GraphQLBoolean":
+      "https://graphql.org/graphql-js/type/#graphqlboolean",
+    "Part-33-GraphQLEnumType":
+      "https://graphql.org/graphql-js/type/#graphqlenumtype",
     "Part-34-GraphQLFloat": "https://graphql.org/graphql-js/type/#graphqlfloat",
     "Part-35-GraphQLID": "https://graphql.org/graphql-js/type/#graphqlid",
-    "Part-36-GraphQLInputObjectType": "https://graphql.org/graphql-js/type/#graphqlinputobjecttype",
+    "Part-36-GraphQLInputObjectType":
+      "https://graphql.org/graphql-js/type/#graphqlinputobjecttype",
     "Part-37-GraphQLInt": "https://graphql.org/graphql-js/type/#graphqlint",
-    "Part-38-GraphQLInterfaceType": "https://graphql.org/graphql-js/type/#graphqlinterfacetype",
+    "Part-38-GraphQLInterfaceType":
+      "https://graphql.org/graphql-js/type/#graphqlinterfacetype",
     "Part-39-GraphQLList": "https://graphql.org/graphql-js/type/#graphqllist",
-    "Part-40-GraphQLNonNull": "https://graphql.org/graphql-js/type/#graphqlnonnull",
-    "Part-41-GraphQLObjectType": "https://graphql.org/graphql-js/type/#graphqlobjecttype",
-    "Part-42-GraphQLScalarType": "https://graphql.org/graphql-js/type/#graphqlscalartype",
-    "Part-43-GraphQLSchema": "https://graphql.org/graphql-js/type/#graphqlschema",
-    "Part-44-GraphQLString": "https://graphql.org/graphql-js/type/#graphqlstring",
-    "Part-45-GraphQLUnionType": "https://graphql.org/graphql-js/type/#graphqluniontype",
-    "Part-46-isAbstractType": "https://graphql.org/graphql-js/type/#isabstracttype",
-    "Part-47-isCompositeType": "https://graphql.org/graphql-js/type/#iscompositetype",
+    "Part-40-GraphQLNonNull":
+      "https://graphql.org/graphql-js/type/#graphqlnonnull",
+    "Part-41-GraphQLObjectType":
+      "https://graphql.org/graphql-js/type/#graphqlobjecttype",
+    "Part-42-GraphQLScalarType":
+      "https://graphql.org/graphql-js/type/#graphqlscalartype",
+    "Part-43-GraphQLSchema":
+      "https://graphql.org/graphql-js/type/#graphqlschema",
+    "Part-44-GraphQLString":
+      "https://graphql.org/graphql-js/type/#graphqlstring",
+    "Part-45-GraphQLUnionType":
+      "https://graphql.org/graphql-js/type/#graphqluniontype",
+    "Part-46-isAbstractType":
+      "https://graphql.org/graphql-js/type/#isabstracttype",
+    "Part-47-isCompositeType":
+      "https://graphql.org/graphql-js/type/#iscompositetype",
     "Part-48-isInputType": "https://graphql.org/graphql-js/type/#isinputtype",
     "Part-49-isLeafType": "https://graphql.org/graphql-js/type/#isleaftype",
     "Part-50-isOutputType": "https://graphql.org/graphql-js/type/#isoutputtype",
     "Part-51-graphql-utilities": "https://graphql.org/graphql-js/utilities/",
-    "Part-52-astFromValue": "https://graphql.org/graphql-js/utilities/#astfromvalue",
-    "Part-53-buildASTSchema": "https://graphql.org/graphql-js/utilities/#buildastschema",
-    "Part-54-buildClientSchema": "https://graphql.org/graphql-js/utilities/#buildclientschema",
-    "Part-55-buildSchema": "https://graphql.org/graphql-js/utilities/#buildschema",
-    "Part-56-introspectionQuery": "https://graphql.org/graphql-js/utilities/#introspectionquery",
-    "Part-57-isValidJSValue": "https://graphql.org/graphql-js/utilities/#isvalidjsvalue",
-    "Part-58-isValidLiteralValue": "https://graphql.org/graphql-js/utilities/#isvalidliteralvalue",
-    "Part-59-printIntrospectionSchema": "https://graphql.org/graphql-js/utilities/#printintrospectionschema",
-    "Part-60-printSchema": "https://graphql.org/graphql-js/utilities/#printschema",
-    "Part-61-typeFromAST": "https://graphql.org/graphql-js/utilities/#typefromast",
+    "Part-52-astFromValue":
+      "https://graphql.org/graphql-js/utilities/#astfromvalue",
+    "Part-53-buildASTSchema":
+      "https://graphql.org/graphql-js/utilities/#buildastschema",
+    "Part-54-buildClientSchema":
+      "https://graphql.org/graphql-js/utilities/#buildclientschema",
+    "Part-55-buildSchema":
+      "https://graphql.org/graphql-js/utilities/#buildschema",
+    "Part-56-introspectionQuery":
+      "https://graphql.org/graphql-js/utilities/#introspectionquery",
+    "Part-57-isValidJSValue":
+      "https://graphql.org/graphql-js/utilities/#isvalidjsvalue",
+    "Part-58-isValidLiteralValue":
+      "https://graphql.org/graphql-js/utilities/#isvalidliteralvalue",
+    "Part-59-printIntrospectionSchema":
+      "https://graphql.org/graphql-js/utilities/#printintrospectionschema",
+    "Part-60-printSchema":
+      "https://graphql.org/graphql-js/utilities/#printschema",
+    "Part-61-typeFromAST":
+      "https://graphql.org/graphql-js/utilities/#typefromast",
     "Part-62-TypeInfo": "https://graphql.org/graphql-js/utilities/#typeinfo",
     "Part-63-graphql-validation": "https://graphql.org/graphql-js/validation/",
-    "Part-64-specifiedRules": "https://graphql.org/graphql-js/validation/#specifiedrules",
-    "Part-65-validate": "https://graphql.org/graphql-js/validation/#validate"
-  }
+    "Part-64-specifiedRules":
+      "https://graphql.org/graphql-js/validation/#specifiedrules",
+    "Part-65-validate": "https://graphql.org/graphql-js/validation/#validate",
+  },
+  graphql_learn_docs: {
+    "Part-0-Introduction": "https://graphql.org//learn/",
+    "Part-1-Queries and Mutations": "https://graphql.org//learn/queries/",
+    "Part-2-Fields": "https://graphql.org//learn/queries/#fields",
+    "Part-3-Arguments": "https://graphql.org//learn/queries/#arguments",
+    "Part-4-Aliases": "https://graphql.org//learn/queries/#aliases",
+    "Part-5-Fragments": "https://graphql.org//learn/queries/#fragments",
+    "Part-6-Operation Name":
+      "https://graphql.org//learn/queries/#operation-name",
+    "Part-7-Variables": "https://graphql.org//learn/queries/#variables",
+    "Part-8-Directives": "https://graphql.org//learn/queries/#directives",
+    "Part-9-Mutations": "https://graphql.org//learn/queries/#mutations",
+    "Part-10-Inline Fragments":
+      "https://graphql.org//learn/queries/#inline-fragments",
+    "Part-11-Schemas and Types": "https://graphql.org//learn/schema/",
+    "Part-12-Type System": "https://graphql.org//learn/schema/#type-system",
+    "Part-13-Type Language": "https://graphql.org//learn/schema/#type-language",
+    "Part-14-Object Types and Fields":
+      "https://graphql.org//learn/schema/#object-types-and-fields",
+    "Part-15-Arguments": "https://graphql.org//learn/schema/#arguments",
+    "Part-16-The Query and Mutation Types":
+      "https://graphql.org//learn/schema/#the-query-and-mutation-types",
+    "Part-17-Scalar Types": "https://graphql.org//learn/schema/#scalar-types",
+    "Part-18-Enumeration Types":
+      "https://graphql.org//learn/schema/#enumeration-types",
+    "Part-19-Lists and Non-Null":
+      "https://graphql.org//learn/schema/#lists-and-non-null",
+    "Part-20-Interfaces": "https://graphql.org//learn/schema/#interfaces",
+    "Part-21-Union Types": "https://graphql.org//learn/schema/#union-types",
+    "Part-22-Input Types": "https://graphql.org//learn/schema/#input-types",
+    "Part-23-Validation": "https://graphql.org//learn/validation/",
+    "Part-24-Execution": "https://graphql.org//learn/execution/",
+    "Part-25-Introspection": "https://graphql.org//learn/introspection/",
+    "Part-26-Introduction": "https://graphql.org//learn/best-practices/",
+    "Part-27-Thinking in Graphs":
+      "https://graphql.org//learn/thinking-in-graphs/",
+    "Part-28-Serving over HTTP":
+      "https://graphql.org//learn/serving-over-http/",
+    "Part-29-Authorization": "https://graphql.org//learn/authorization/",
+    "Part-30-Pagination": "https://graphql.org//learn/pagination/",
+    "Part-31-Global Object Identification":
+      "https://graphql.org//learn/global-object-identification/",
+    "Part-32-Caching": "https://graphql.org//learn/caching/",
+  },
+};
